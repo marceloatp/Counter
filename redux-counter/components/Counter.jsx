55928af96c6a33948213ca9a8c5265ada8503557
Marcelo Pereira <marceloatp@gmail.com>
2024-01-31 17:58:24 +0000

    Adding Component Counter

---
+++ redux-counter/components/Counter.jsx
@@ -0,0 +1,6 @@
+export function Counter () {
+
+    return (
+        <div>counter</div>
+    )
+}
--- redux-counter/src/App.jsx
+++ redux-counter/src/App.jsx
@@ -1,6 +1,10 @@
+import { Counter } from "../components/Counter"
+
 const App = () => (
   <main className="w-48 m-auto flex-col gap-8">
     <h1 className="font-semibold text-center border-b p-2">Redux Counter</h1>
+    
+    <Counter />
   </main>
 )
 
