48af343378ebc1f134ae7f000232731b0d06c080
Marcelo Pereira <marceloatp@gmail.com>
2024-01-31 18:34:49 +0000

    Adding style to buttons

--- redux-counter/components/CounterActions.jsx
+++ redux-counter/components/CounterActions.jsx
@@ -3,13 +3,13 @@ import { Plus } from "@phosphor-icons/react/dist/ssr";
 
 export function CounterActions() {
     return (
-        <div>
-            <button>
-                <Plus weight="bold"/>
-            </button>
-            <button>
-                <Minus weight="bold"/>
-            </button>
+        <div className="flex flex-col">
+             <button className="bg-zinc-200 p-4 flex items-center justify-center border-b border-zinc-300 hover:bg-zinc-100 transition-colors">
+                    <Plus weight="bold" className="text-zinc-500" />
+                </button>
+                <button className="bg-zinc-200 p-4 flex items-center justify-center hover:bg-zinc-100 transition-colors">
+                    <Minus weight="bold" className="text-zinc-500" />
+                </button>
         </div>
     )
 }
--- redux-counter/components/CounterDisplay.jsx
+++ redux-counter/components/CounterDisplay.jsx
@@ -5,7 +5,7 @@ export class CounterDisplay extends Component {
     render() {
         return(
             <h2 className="font-semibold text-4xl p-5">
-            0
+                0
             </h2>
         )
     }
